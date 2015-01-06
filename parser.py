84d3f880d067f4e515c5194536daf1d671a463ea
Jordan <j6k4m8@gmail.com>
2015-01-06 17:26:00 -0500

    Automatically output keys for use in dumper

--- parser.py
+++ parser.py
@@ -9,8 +9,8 @@ __credits__         = ["Jordan Matelsky", "Raghav Sood"]
 __license__         = "CC"
 
 
-if 1 <= len(sys.argv) < 2:
-    print "Usage:\n    python parser.py [response_file]\n" + \
+if 1 <= len(sys.argv) > 3:
+    print "Usage:\n    python parser.py [response_file [output_file]]\n" + \
           "After you have found thread_info.php in the Network tab of \n" +  \
           "Chrome Developer Tools, click on the Response tab and paste \n" + \
           "everything from that tab into a file `response_file`."
@@ -19,22 +19,24 @@ if 1 <= len(sys.argv) < 2:
 with open(sys.argv[1], "r") as response_file:
     response = response_file.read()
 
-    print response
     # Response = curl 'https://www.facebook.com...' with all vars stored
     # in the string in many different ways. We've gotta pull them out...
-    __user = re.search(r"user=(\w+)", response).group(1)
-    cookie = re.search(r"'cookie: (.+?)'", response).group(1)
-    __a = re.search(r"__a=(.+?)&", response).group(1)
-    __dyn = re.search(r"__dyn=(.+?)&", response).group(1)
-    __req = re.search(r"__req=(.+?)&", response).group(1)
-    fb_dtsg = re.search(r"fb_dtsg=(.+?)&", response).group(1)
-    ttstamp = re.search(r"ttstamp=(.+?)&", response).group(1)
-    __rev = re.search(r"__rev=(.+?)'", response).group(1)
+    values = {}
+    values["__user"] = re.search(r"user=(\w+)", response).group(1)
+    values["cookie"] = re.search(r"'cookie: (.+?)'", response).group(1)
+    values["__a"] = re.search(r"__a=(.+?)&", response).group(1)
+    values["__dyn"] = re.search(r"__dyn=(.+?)&", response).group(1)
+    values["__req"] = re.search(r"__req=(.+?)&", response).group(1)
+    values["fb_dtsg"] = re.search(r"fb_dtsg=(.+?)&", response).group(1)
+    values["ttstamp"] = re.search(r"ttstamp=(.+?)&", response).group(1)
+    values["__rev"] = re.search(r"__rev=(.+?)'", response).group(1)
 
 
-    with open("_response_values", 'w') as export:
-        export.write(cookie + '\n');  export.write(__user + '\n');
-        export.write(__a + '\n');  export.write(__dyn + '\n');
-        export.write(__req + '\n');  export.write(fb_dtsg( + '\n');
-        export.write(ttstamp + '\n');  export.write(__rev + '\n');
+    output_file ="_response_values"
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+   
+    with open(output_file, 'w') as export:
+        for (i, v) in values.iteritems():
+            export.write(i + "\n" + v + "\n\n")
 
